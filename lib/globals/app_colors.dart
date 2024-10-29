968ba4b45d80e84de99af152bcf565c663983116
PhamThanhDat <104967900+datonefeed@users.noreply.github.com>
2024-10-30 03:42:03 +0700

    feat: ceate colors theme

---
+++ lib/globals/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color bgColor = const Color(0xff1f232d);
+  static Color themeColor = Color.fromARGB(255, 236, 42, 107);
+  static Color aqua = Color.fromARGB(255, 250, 0, 175);
+  static Color lawGreen = Color.fromARGB(255, 118, 0, 252);
+  static Color bgColor2 = Color(0xff313743);
+  static Color robinEdgeBlue = Color.fromARGB(255, 236, 42, 107);
+  static Color white = const Color(0xffffffff);
+}
